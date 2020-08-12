bdf19b3e7c3d30e01d1d5fb7585c44fa3bcb39be
Wangxq <3024881272@qq.com>
2020-08-12 16:32:02 +0800

    package scroll components init&&data

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -19,6 +19,12 @@ export default {
       type: Boolean,
       default: false,
     },
+    data: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
   },
   data() {
     return {
@@ -27,20 +33,7 @@ export default {
   },
   mounted() {
     // 1.创建 BScroll 对象
-    this.scroll = new BScroll(this.$refs.wrapper, {
-      mouseWheel: true,
-
-      //  表单元素点击可以监听，但是 div 等其他元素需要设置为 ture
-      click: true,
-
-      // 0,1都是不侦测实时的位置
-      // 2: 在手指滚动的过程中侦测, 手指离开后的惯性滚动过程中不侦测.
-      // 3: 只要是滚动, 都侦测
-      probeType: this.probeType,
-
-      // pullup 插件相关配置，true 监听上拉加载更多
-      pullUpLoad: this.pullUpLoad,
-    });
+    setTimeout(this.__initScroll, 20);
     // 2.监听滚动事件
     // BetterScroll 实时派发 scroll 事件，是需要设置 probeType 为 3。
     this.scroll.on("scroll", (position) => {
@@ -54,6 +47,24 @@ export default {
     });
   },
   methods: {
+    __initScroll() {
+      // 1.初始化BScroll对象
+      if (!this.$refs.wrapper) return;
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        mouseWheel: true,
+
+        //  表单元素点击可以监听，但是 div 等其他元素需要设置为 ture
+        click: true,
+
+        // 0,1都是不侦测实时的位置
+        // 2: 在手指滚动的过程中侦测, 手指离开后的惯性滚动过程中不侦测.
+        // 3: 只要是滚动, 都侦测
+        probeType: this.probeType,
+
+        // pullup 插件相关配置，true 监听上拉加载更多
+        pullUpLoad: this.pullUpLoad,
+      });
+    },
     // 封装 scrollTo 方法，让 Home 组件直接调用
     // ES6 新语法 参数可以赋默认值
     scrollTo(x, y, time = 300) {
@@ -77,6 +88,11 @@ export default {
      */
     getScrollY() {
       return this.scroll ? this.scroll.y : 0;
+    },
+  },
+  watch: {
+    data() {
+      setTimeout(this.refresh, 20);
     },
   },
 };
