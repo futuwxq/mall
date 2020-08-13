623a666fbbc250eecd84110eeee6b275323aaa59
Wangxq <3024881272@qq.com>
2020-08-13 11:54:29 +0800

    add tabControlMixin

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -1,23 +1,23 @@
-import { debounce } from "common/utils";
+// import { debounce } from "common/utils";
 // 定义一个混入对象
 // 组件中的 option 都可以写入混入，当出现同名选项的时候，会进行合并
-export const itemListenerMixin = {
-    data() {
-        return {
-            itemImageListener: null,
-            newRefresh: null
-        }
-    },
-    mounted() {
-        // 监听商品 item 中图片加载完成
-        this.newRefresh = debounce(this.$refs.scroll.refresh, 100);
-        // 对监听事件进行保存
-        this.itemImageListener = () => {
-            this.newRefresh();
-        };
-        this.$bus.$on("itemImageLoad", this.itemImageListener);
-    }
-}
+// export const itemListenerMixin = {
+//     data() {
+//         return {
+//             itemImageListener: null,
+//             newRefresh: null
+//         }
+//     },
+//     mounted() {
+//         // 监听商品 item 中图片加载完成
+//         this.newRefresh = debounce(this.$refs.scroll.refresh, 100);
+//         // 对监听事件进行保存
+//         this.itemImageListener = () => {
+//             this.newRefresh();
+//         };
+//         this.$bus.$on("itemImageLoad", this.itemImageListener);
+//     }
+// }
 import BackTop from "components/content/backTop/BackTop";
 import { BACK_POSITION } from "common/const";
 export const backTopMixin = {
@@ -52,7 +52,7 @@ export const tabControlMixin = {
         }
     },
     methods: {
-        titleClick(index) {
+        tabClick(index) {
             // console.log(index);
             switch (index) {
                 case 0:
